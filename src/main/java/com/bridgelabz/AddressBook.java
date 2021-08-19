fbc81b8d58273932a823b7de51f4866e206098f9
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-19 14:49:22 +0530

    ADD: Create Contacts in Address Book

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
+/* @Description - To create a contacts in address book with first name, last name, address, city, state,
+ * zip,mobile number.*/
 public class AddressBook {
-    public static void main(String[] args){
-        System.out.println("Welcome to Address Book!");
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book system program");
+        Contacts contact = new Contacts("Gagan", "Reddy", "Heelalige",
+                "Benglore", "Karnataka", 560099,
+                918722289619L, "gagan@gmail.com");
+        System.out.println(contact.toString());
     }
 }
